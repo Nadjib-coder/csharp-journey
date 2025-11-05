cb3c8ed751bfdb4ed87339649f7023584135d059
Nadjib Mouhoun <nadjibmouhoun1@gmail.com>
2025-11-05 11:22:43 +0000

    understand the static method

---
+++ ConsoleApp1/Calc/Calculator.cs
@@ -0,0 +1,12 @@
+namespace ConsoleApp1.Calc;
+
+public class Calculator
+{
+    public static int Add(int x, int y)
+    {
+        int Num1 = x;
+        int Num2 = y;
+        int result = x + y;
+        return result;
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,7 @@
 ﻿using ConsoleApp1.People;
+using ConsoleApp1.Calc;
 namespace ConsoleApp1;
+
 
 class Program
 {
@@ -9,12 +11,16 @@ class Program
         // name = name.ToUpper();
         // Console.WriteLine(name);
 
+        /*
         Person me = new Person();
 
         me.Name = "Nadjib";
         me.Age = 23;
         me.HasPet = true;
         me.Greeting();
+        */
+        int sum = Calculator.Add(23, 19);
+        Console.WriteLine(sum);
     }
 }
 
